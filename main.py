4d6653029f27062602d63d146f0d13ea38d24bd3
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-03-01 10:22:15 +0200

    removed connection to the top

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import mysql.connector
 import sys
 
+try:
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='root',
+        password='MetroSuomi2024',  # change it to your password
+        autocommit=True
+    )
+    print("Database connected successfully!")   # we can comment this line later
+except mysql.connector.Error as error:
+    print("Error while connecting to MySQL:", error)
+    sys.exit(1)  # Exit the program with a non-zero status code indicating an error
 
 # Reads user input for the airport. Returns ICAO code of the selected airport.
 def airport_input() -> str:
@@ -81,17 +94,3 @@ def database_query(sql: str) -> list:
 
     cursor.close()
     return result
-
-try:
-    connection = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='flight_game',
-        user='root',
-        password='MetroSuomi2024',  # change it to your password
-        autocommit=True
-    )
-    print("Database connected successfully!")   # we can comment this line later
-except mysql.connector.Error as error:
-    print("Error while connecting to MySQL:", error)
-    sys.exit(1)  # Exit the program with a non-zero status code indicating an error
